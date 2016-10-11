fad7090cdc19b6b3580a1921259d3b20d79572ea
Salvador Roibon <sroibon@tradehelm.com>
2016-10-11 15:37:24 -0300

    update server file

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(3002, function () {
+app.listen(process.env.PORT || 3002, function () {
   console.log('Example app listening on port 3002!');
 });
